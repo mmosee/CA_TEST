72b9d15127a6018f581a48e21298df7c302aad87
mmosee <nammosee@gmail.com>
2024-05-03 13:36:20 +0900

    js file 경로

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const path = require('path');
+const axios = require('axios');
+
 const port = 7700;
 
-// index.html 파일을 제공합니다.
-app.get('/', (req, res) => {
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(cors());
 
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html")
 });
 
-
-// config.json의 execute, save, publish, validate, stop
+// config.json에서 정의한 execute, save, publish, validate, stop의 EndPoint
 app.post('/execute', function(req, res) {
     console.log('debug: /execute');
+    
     console.log(' req.body', JSON.stringify(req.body));
     
     return res.status(200).json({});
@@ -21,30 +26,26 @@ app.post('/execute', function(req, res) {
 
 app.post('/save', function(req, res) {
     console.log('debug: /save');
-    
     return res.status(200).json({});
 });
 
 app.post('/publish', function(req, res) {
     console.log('debug: /publish');
-    
     return res.status(200).json({});
 });
 
 app.post('/validate', function(req, res) {
     console.log('debug: /validate');
-    
     return res.status(200).json({});
 });
+
 
 app.post('/stop', function(req, res) {
     console.log('debug: /stop');
-    
     return res.status(200).json({});
 });
 
-
-// 서버를 시작합니다.
+// listen
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+    console.log(`Started ${port}`);
+})
