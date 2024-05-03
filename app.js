e285ef7d93bde792ef36a9401118edf05027081d
mmosee <nammosee@gmail.com>
2024-05-03 13:27:54 +0900

    endPoint URL modify

--- app.js
+++ app.js
@@ -10,6 +10,40 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+
+// config.json의 execute, save, publish, validate, stop
+app.post('/execute', function(req, res) {
+    console.log('debug: /execute');
+    console.log(' req.body', JSON.stringify(req.body));
+    
+    return res.status(200).json({});
+});
+
+app.post('/save', function(req, res) {
+    console.log('debug: /save');
+    
+    return res.status(200).json({});
+});
+
+app.post('/publish', function(req, res) {
+    console.log('debug: /publish');
+    
+    return res.status(200).json({});
+});
+
+app.post('/validate', function(req, res) {
+    console.log('debug: /validate');
+    
+    return res.status(200).json({});
+});
+
+app.post('/stop', function(req, res) {
+    console.log('debug: /stop');
+    
+    return res.status(200).json({});
+});
+
+
 // 서버를 시작합니다.
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
