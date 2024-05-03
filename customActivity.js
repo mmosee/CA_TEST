1f030afc2448426a6374389dedcf1dd4992c677c
mmosee <nammosee@gmail.com>
2024-05-03 16:24:55 +0900

    fetch get은 header가 안먹는데

--- customActivity.js
+++ customActivity.js
@@ -59,7 +59,78 @@ define(["postmonger"], function (Postmonger) {
         connection.trigger("updateActivity", payload);
     }
 
-    function getMessage() {
-        return $("#select1").find("option:selected").attr("value").trim();
+
+    // Test const
+    
+
+    // Test document
+    $(document).on('click', '#getBtn', getBtn)
+
+    // Test Function
+    function getBtn() {
+        const getInput = $('#getInput').val();
+        console.log("getBtn", getInput);
+
+        const testData = {
+            test: 'test'
+        }
+
+        // F T
+        // fetch("https://server-test-ugbd.onrender.com/input", {
+        //     method: "GET",
+        //     body: JSON.stringify({
+        //       "test" : "test",
+        //     }),
+        //   })
+        //   .then((response) => response.json())
+        //   .then((result) => console.log(result));
+
+        // FETCH TEST POST
+        // fetch('https://server-test-ugbd.onrender.com/input', {method: 'POST', headers: {'Content-Type': 'application/json',}, body: JSON.stringify(testData),})
+        // .then(response => {if (!response.ok) throw new Error('Network response was not ok');return response.json();})
+        // .then(data => {
+        //     console.log('서버 응답:', data);
+        // })
+        // .catch(error => {
+        //     console.error('에러 발생:', error);
+        // });
+
+        // FETCH TEST GET
+        fetch('https://server-test-ugbd.onrender.com/input?test=test', {
+            method: 'GET', 
+            headers: {
+                'userId': 'testUserId',
+            },
+        })
+        .then(response => {if (!response.ok) throw new Error('Network response was not ok');return response.json();})
+        .then(data => {
+            console.log('서버 응답:', data);
+        })
+        .catch(error => {
+            console.error('에러 발생:', error);
+        });
+
+
+        // AJAX TEST
+        // $.ajax({
+        //     url: 'https://server-test-ugbd.onrender.com/input',
+        //     method: 'get',
+        //     data: {
+        //         getInput: getInput
+        //     },
+        //     dataType: 'json',
+        //     success: function (data, status, xhr) {
+        //         console.log('ajax - success');
+        //         console.log("data : : " + JSON.stringify(data));
+        //     },
+        //     error: function (data, status, err) {
+        //         console.log('ajax - error');
+        //     },
+        //     complete: function () {
+        //         console.log('ajax - complete');
+        //     }
+        // });
+
     }
+
 });
